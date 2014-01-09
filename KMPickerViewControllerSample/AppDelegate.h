2ba159876ced7de7792207f31d9594275c0fa288
matsuda <3matsuda@gmail.com>
2014-01-09 19:16:02 +0900

    create project

---
+++ KMPickerViewControllerSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  KMPickerViewController
+//
+//  Created by Kosuke Matsuda on 2014/01/09.
+//  Copyright (c) 2014年 matsuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ KMPickerViewControllerTests/KMPickerViewControllerTests.m
@@ -0,0 +1,34 @@
+//
+//  KMPickerViewControllerTests.m
+//  KMPickerViewControllerTests
+//
+//  Created by Kosuke Matsuda on 2014/01/09.
+//  Copyright (c) 2014年 matsuda. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface KMPickerViewControllerTests : XCTestCase
+
+@end
+
+@implementation KMPickerViewControllerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
